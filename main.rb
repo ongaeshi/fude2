873a9ce6801e8c728fdf031b091f6f6b2ad18ee7
ongaeshi <ongaeshi0621@gmail.com>
2020-02-20 00:18:26 +0900

    feat: Convert to 256x256

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ script do
   loop do
     @i = 0
 
-    @text = "色番号のサンプルです。あああああああ"
+    @text = "色番号のサンプルです。"
     yld 120
     @text = ""
 
@@ -20,14 +20,13 @@ end
 draw do
   cls
   tile(@i)
-  if @text != ""
-    rect(16 - 2, 64 - 4 - 2, 128 - 32 + 4, 16 + 8, 7)
-    printrec(@text, 16, 64 - 4, 128 - 24, 16 + 4, 7)
-  end
+  print(@text, 32, 128 - 8, 7)
 end
 
 def tile_n(x, y, n)
-  rectfill x, y, 32, 32, n
+  x *= 64
+  y *= 64
+  rectfill x, y, 64, 64, n
   c = n >= 6 ? 0 : 7
   a = ["黒", "濃紺", "紫", "緑", "茶", "泥", "灰", "白", "赤", "橙", "黄", "黄緑", "青", "藍", "淡紅", "桃"]
   print "#{n}\n#{a[n]}", x + 12, y + 4, c
@@ -37,35 +36,35 @@ def tile(n)
   return if n == 0
   tile_n(0, 0, 0)
   return if n == 1
-  tile_n(32, 0, 1)
+  tile_n(1, 0, 1)
   return if n == 2
-  tile_n(64, 0, 2)
+  tile_n(2, 0, 2)
   return if n == 3
-  tile_n(96, 0, 3)
+  tile_n(3, 0, 3)
   return if n == 4
-  tile_n(0, 32, 4)
+  tile_n(0, 1, 4)
   return if n == 5
-  tile_n(32, 32, 5)
+  tile_n(1, 1, 5)
   return if n == 6
-  tile_n(64, 32, 6)
+  tile_n(2, 1, 6)
   return if n == 7
-  tile_n(96, 32, 7)
+  tile_n(3, 1, 7)
   return if n == 8
-  tile_n(0, 64, 8)
+  tile_n(0, 2, 8)
   return if n == 9
-  tile_n(32, 64, 9)
+  tile_n(1, 2, 9)
   return if n == 10
-  tile_n(64, 64, 10)
+  tile_n(2, 2, 10)
   return if n == 11
-  tile_n(96, 64, 11)
+  tile_n(3, 2, 11)
   return if n == 12
-  tile_n(0, 96, 12)
+  tile_n(0, 3, 12)
   return if n == 13
-  tile_n(32, 96, 13)
+  tile_n(1, 3, 13)
   return if n == 14
-  tile_n(64, 96, 14)
+  tile_n(2, 3, 14)
   return if n == 15
-  tile_n(96, 96, 15)
+  tile_n(3, 3, 15)
 end
 
-run("fude", 2)
+run("fude", 1)
