ffa7f42ee3979a7c1c6b6ecbf705f4ce7362b41b
ongaeshi <ongaeshi0621@gmail.com>
2020-02-20 02:07:45 +0900

    feat: Tweak

--- array_animation.rb
+++ array_animation.rb
@@ -1,21 +1,10 @@
 require "./fude"
 
-def run_command(command)
-  1.upto(command.length) do |e|
-    @text = command[0..e]
-    @color = 0
-    yld 1
-  end
-
-  yld 15
-  @color = 7
-  yield
-  yld 30
-end
-
 script do
   loop do
+    @text_offx = 0
     @array = []
+    @array_offx = 0
 
     run_command("x = [1, 2, 3]") do
       @array = [1, 2, 3]
@@ -25,8 +14,25 @@ script do
       @array.push(4)
     end
 
+    run_command("x.unshift(5)") do
+      @array_offx = -32
+      @array.unshift 5
+    end
+
     run_command("x.reverse!") do
       @array.reverse!
+    end
+
+    run_command("x = x.map { |e| e * 2 }", -32) do
+      @array = @array.map { |e| e * 2 }
+    end
+
+    run_command("x = x.fill(9)") do
+      @array = @array.fill(9)
+    end
+
+    run_command("x.delete(9)") do
+      @array.delete(9)
     end
 
     yld 30
@@ -35,13 +41,30 @@ end
 
 draw do
   cls 13
-  print(@text, 64, 32, @color)
+  print(@text, 64 + @text_offx, 32, @color)
 
   (0...@array.length).each do |i|
-    x = 64 + 32 * i - 1
+    x = 64 + 32 * i - 1 + @array_offx
     rect(x, 64, 32, 32, 7)
     print(@array[i].to_s, x + 12, 64 + 8, 7)
   end
+end
+
+def run_command(command, offx = 0)
+  @text = ""
+  @text_offx = offx
+
+  a = command.split("")
+  a.each do |e|
+    @text += e
+    @color = 0
+    yld 4
+  end
+
+  yld 30
+  @color = 7
+  yield
+  yld 30
 end
 
 run("array animation")
